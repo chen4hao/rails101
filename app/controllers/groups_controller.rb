2f6e294af151f920f42f0f793d2532f63f0a9481
chen4hao <steven.chen4hao@gmail.com>
2015-12-12 09:18:21 +0800

    Install/setup simple_form gem and create 7 groups actions/views

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,4 +4,28 @@ class GroupsController < ApplicationController
     flash[:warning] = "這是 warning 訊息"
 
   end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
 end

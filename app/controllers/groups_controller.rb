377056c19c813f045ec9ea7a91d7e209b35f6bc9
chen4hao <steven.chen4hao@gmail.com>
2015-12-14 11:26:32 +0800

    Update only owner/author can update/delete group and post

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,7 +10,7 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = Group.create(group_params)
+    @group = current_user.groups.create(group_params)
     if @group.save
       redirect_to groups_path, notice: '新增討論版成功'
     else
@@ -26,12 +26,12 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
+    @group = current_user.groups.find(params[:id])
 
   end
 
   def update
-    @group = Group.find(params[:id])
+    @group = current_user.groups.find(params[:id])
 
     if @group.update(group_params)
       redirect_to groups_path, notice: '修改討論版成功'
@@ -42,7 +42,7 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
+    @group = current_user.groups.find(params[:id])
 
     @group.destroy
     redirect_to groups_path, alert: '討論版已刪除'
